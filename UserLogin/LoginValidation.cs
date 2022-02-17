90fae4afb69d7fef09234d8e52bdc46f5ffd8b6c
ivan.stoianov <ivan.stoianov@sap.com>
2022-02-17 12:00:02 +0200

    UserRoles has been added

--- UserLogin/LoginValidation.cs
+++ UserLogin/LoginValidation.cs
@@ -3,12 +3,15 @@ namespace UserLogin
 {
     public class LoginValidation
     {
+        public static UserRoles currentUserRole;
+
         public LoginValidation()
         {
         }
 
         public bool ValidateUserInput()
         {
+            currentUserRole = UserRoles.ADMIN;
             return true;
         }
     }
