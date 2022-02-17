e54b6c920ec60461226b6aec37bbf8e2bf054777
ivan.stoianov <ivan.stoianov@sap.com>
2022-02-17 11:18:59 +0200

    user class has been added

---
+++ UserLogin/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserLogin
+{
+    public class User
+    {
+        public String Username;
+
+        public String password;
+
+        public String FakNum;
+
+        public Int32 Role;
+
+        public User()
+        {
+           
+        }
+    }
+}
